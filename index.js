86999afd6f6deca58821365505aca511e3722587
Richard Parker <rlparker.uk@gmail.com>
2017-02-21 18:51:26 +0000

    Added stuff

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+// serve static assets normally
+app.use(express.static(__dirname + '/public'))
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
