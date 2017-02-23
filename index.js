48e7de8500a18a773cd5e987b0fadd4402dd8cc3
Richard Parker <rlparker.uk@gmail.com>
2017-02-23 11:08:02 +0000

    Update main express file

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
 var path = require('path');
 var express = require('express');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+require('./models/db');
+
+var routes = require('./routes/index');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-// serve static assets normally
-app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
