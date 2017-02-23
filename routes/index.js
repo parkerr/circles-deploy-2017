79163ec692f431fedabca220af4af52895e34acc
Richard Parker <rlparker.uk@gmail.com>
2017-02-23 10:13:15 +0000

    Tidy up route

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-var ctrlSpaces = require('../controllers/spaces');
+var ctrlNicknames = require('../controllers/nicknames');
 
-//spaces
-router.get('/spaces', ctrlSpaces.getAllSpaces);
-router.get('/spaces/:spacenumber', ctrlSpaces.getOneSpace);
-router.put('/spaces/:spacenumber', ctrlSpaces.updateOneSpace);
-router.put('/spaces/:spacenumber/makeAvailableOn/:date', ctrlSpaces.makeSpaceAvailable);
-
-router.put('/spaces/:spacenumber/makeunAvailableOn/:date', ctrlSpaces.makeSpaceUnavailable);
-//router.post('/spaces', ctrlSpaces.createNextSpace);
+//Nicknames
+router.get('/nicknames', ctrlSpaces.getAllNicknames);
+router.get('/nicknames/:nickname', ctrlSpaces.getOneNickname);
+router.put('/nicknames/:nickname', ctrlSpaces.updateOneNickname);
 
 module.exports = router;
