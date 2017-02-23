1b2543c5f7d77c9c574d2f542cd30f71e95cc13e
Richard Parker <rlparker.uk@gmail.com>
2017-02-23 10:04:29 +0000

    Add Controller, model and routes files

---
+++ controllers/nicknames.js
@@ -0,0 +1,70 @@
+var mongoose = require('mongoose');
+var spaces = mongoose.model('Space');
+
+
+var getAll = function(req, res){
+  spaces.find().sort({number: 1}).exec(function(err, space){
+    if(!space.length){
+      sendJsonResponse(res, 404, {"message" : "no spaces found"});
+      return
+    } else if(err){
+      sendJsonResponse(res, 404, err);
+      return
+    }
+    sendJsonResponse(res, 200, space);
+  });
+};
+
+
+module.exports.getAllSpaces = function(req, res){
+  getAll(req, res);
+};
+
+module.exports.getOneSpace = function(req, res) {
+  if (req.params && req.params.spacenumber){
+    spaces.find({number : req.params.spacenumber}).exec(function(err, space){
+      if(!space.length){
+		  sendJsonResponse(res, 404, {"message" : "space number not found"});
+          return
+	  } else if(err){
+          sendJsonResponse(res, 404, err);
+          return
+      }
+      sendJsonResponse(res, 200, space);
+    });
+  } else {
+    sendJsonResponse(res, 404, {"message" : "no space number in request"});
+  }    
+};
+
+module.exports.makeSpaceUnavailable = function(req, res){
+    spaces.findOneAndUpdate({number: req.params.spacenumber}, {$pull: {availableOn: req.params.date}}, {new: true}, function(err, space){
+	  getAll(req, res);
+    });
+};
+
+module.exports.makeSpaceAvailable = function(req, res){
+    spaces.findOneAndUpdate({number: req.params.spacenumber}, {$push: {availableOn: req.params.date}}, {new: true}, function(err, space){
+	  getAll(req, res);
+    });
+};
+
+
+module.exports.updateOneSpace = function(req, res) {
+};
+
+
+module.exports.createSpace = function(req, res){
+  spaces.create({number: 1}, function(err, card){
+    sendJsonResponse(res, 200, card);
+  });
+};
+
+
+var sendJsonResponse = function(res, status, content){
+  res.status(status);
+  res.json(content);
+};
+
+
+
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+var ctrlSpaces = require('../controllers/spaces');
+
+//spaces
+router.get('/spaces', ctrlSpaces.getAllSpaces);
+router.get('/spaces/:spacenumber', ctrlSpaces.getOneSpace);
+router.put('/spaces/:spacenumber', ctrlSpaces.updateOneSpace);
+router.put('/spaces/:spacenumber/makeAvailableOn/:date', ctrlSpaces.makeSpaceAvailable);
+
+router.put('/spaces/:spacenumber/makeunAvailableOn/:date', ctrlSpaces.makeSpaceUnavailable);
+//router.post('/spaces', ctrlSpaces.createNextSpace);
+
+module.exports = router;
